699e5752ee1c658a322df51f022fe6b7bb035174
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2020-06-03 13:11:32 +0100

    5356: Added inactive "Active" issues to reports. Summary to admins.

--- lib/redmineResolvedReminder/constants.rb
+++ lib/redmineResolvedReminder/constants.rb
@@ -2,7 +2,7 @@ module RedmineResolvedReminder
   module Constants
     VERSION = "1.0.2"
     PACKAGE = "redmineResolvedReminder"
-    RECEIVE_ORPHANS = ["eldrid01","stark01","brown22"]
-    ISSUE_URL = "http://redmine-bioinformatics.cruk.cam.ac.uk:3000/issues/"
+    RECEIVE_ORPHANS = ["eldrid01","stark01","brown22","bowers01"]
+    ISSUE_URL = "https://redmine-bioinformatics.cruk.cam.ac.uk/issues/"
   end
 end
--- lib/redmineResolvedReminder/resolvedReminder.rb
+++ lib/redmineResolvedReminder/resolvedReminder.rb
@@ -26,6 +26,35 @@ module RedmineResolvedReminder
       @log = Logger.new(STDERR)
       @log.level = @testMode ? Logger::DEBUG : Logger::INFO
     end
+    
+    # Get title, email addr of assigned-to person, links to watchers of the issue
+    # (should we also email the originator, if different?)
+    def processIssue(issue)
+      assn = issue.assigned_to
+      orig = issue.author
+      if assn.nil? || assn.status != 1 # '1' is 'active', '3' is 'disabled'
+        if orig.nil? || orig.status != 1
+          receiver = nil
+        else
+          receiver = orig
+        end
+      else
+        receiver = assn
+      end
+      if receiver.nil?
+        @orphanIssueSet << issue
+      else
+        target = receiver.email_address.address
+        if ! @userIssueSet.has_key?(target)
+          @userIssueSet[target] = []
+          @userLoginSet[target] = receiver.login
+        end
+        @userIssueSet[target] << issue
+      end
+      unless issue.watcher_user_ids.nil?
+        @watcherIssueSet[issue.id] = User.where(id: issue.watcher_user_ids)
+      end
+    end
 
     def collectIssues
       # retrieve list of issues in "Resolved" state
@@ -34,28 +63,18 @@ module RedmineResolvedReminder
       status = IssueStatus.find_by(name: "Resolved")
       @log.debug "Status: #{status.name} (#{status.id})"
       Issue.where(status: status).each do |issue|
-        assn = issue.assigned_to
-        orig = issue.author
-        if assn.nil? || assn.status != 1 # '1' is 'active', '3' is 'disabled'
-          if orig.nil? || orig.status != 1
-            receiver = nil
-          else
-            receiver = orig
-          end
-        else
-          receiver = assn
-        end
-        if receiver.nil?
-          @orphanIssueSet << issue
-        else
-          target = receiver.email_address.address
-          if ! @userIssueSet.has_key?(target)
-            @userIssueSet[target] = []
-            @userLoginSet[target] = receiver.login
-          end
-          @userIssueSet[target] << issue
-        end
+        processIssue(issue)
       end
+      
+      # Similar thing for issues that are in the active state but the last
+      # update was more than 60 days ago.
+      lowerbound = Date.new(2000, 1, 1)
+      cutoff = Date.today - 60
+      status = IssueStatus.find_by(name: "Active")
+      Issue.where(status: status, updated_on: lowerbound..cutoff).each do |issue|
+        processIssue(issue)
+      end
+      
     end
 
     def loadTemplate(fn)
@@ -84,11 +103,12 @@ module RedmineResolvedReminder
         @issList = issues
         renderer = ERB.new(template,nil,">")
         body = renderer.result(binding)
-        mailReport(body,@email,issues.length)
+        subject = "Resolved/inactive issues: #{@issList.length}"
+        mailReport(subject, body, @email)
       end
       # this next clause covers the case where Rory or Matt has no assigned
       # issues, but they should still receive the orphans if any.
-      if @orphanIssueSet.length > 0
+      if !@orphanIssueSet.empty?
         RECEIVE_ORPHANS.each do |username|
           if ! @userLoginSet.has_value?(username)
             @email = User.find_by(login: username).email_address.address
@@ -96,13 +116,25 @@ module RedmineResolvedReminder
             @issList = []
             renderer = ERB.new(template,nil,">")
             body = renderer.result(binding)
-            mailReport(body,@email,@issList.length)
+            subject = "Resolved/inactive issues: #{@issList.length}"
+            mailReport(subject, body, @email)
           end
+        end
+      end
+      # This clause sends the summary report to the Redmine admins.
+      if !(@userIssueSet.empty? && @orphanIssueSet.empty?)
+        template = loadTemplate("report.txt.erb")
+        renderer = ERB.new(template,nil,">")
+        body = renderer.result(binding)
+        subject = "Resolved/inactive issue summary for #{@date}"
+        RECEIVE_ORPHANS.each do |username|
+          @email = User.find_by(login: username).email_address.address
+          mailReport(subject, body, @email)
         end
       end
     end
 
-    def mailReport(text,recip,count)
+    def mailReport(subjectLine, text, recip)
       mailhost = Redmine::Configuration['email_delivery']['smtp_settings'][:address]
       mailport = Redmine::Configuration['email_delivery']['smtp_settings'][:port]
       maildomain = Redmine::Configuration['email_delivery']['smtp_settings'][:domain]
@@ -118,13 +150,14 @@ module RedmineResolvedReminder
       Mail.deliver do
         from    "resolvedReport@#{Socket.gethostname}"
         to      recip
-        subject "resolved issues: #{count}"
+        subject subjectLine
         body    text
       end
     end
 
     def run
       @userIssueSet = {}
+      @watcherIssueSet = {}
       @userLoginSet = {}
       @orphanIssueSet = []
       collectIssues
